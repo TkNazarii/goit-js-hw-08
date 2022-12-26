83f3ff3427b92235010ae68f74c110b627a874c7
TkNazarii <tknazkooo@gmail.com>
2022-12-27 01:37:05 +0200

    second

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import simpleLightbox from "simplelightbox";
+console.log(galleryItems);
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+console.log(simpleLightbox);
 // Change code below this line
 
-console.log(galleryItems);
+
+// посилання на контейнер
+const galleryContainer = document.querySelector(".gallery");
+
+// роммітка для формування
+function createPictureMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div> 
+        `;
+    })
+    .join("");
+}
+
+const cardsMarkup = createPictureMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+
+player.on('play', function() {
+	console.log('Пуск відео');
+});
+
+player.on('timeupdate', function(data) {
+	console.log('Тривалість: '+data.seconds);
+	const asd = +data.seconds
+	localStorage.setItem("videoplayer-current-time", JSON.stringify(asd));
+});
+
+const savedSettings = localStorage.getItem("videoplayer-current-time");
+const parsedSettings = JSON.parse(savedSettings);
+console.log(parsedSettings); 
+
+player.setCurrentTime(parsedSettings);
+
+
+
+
+// (function(){
+// 	try{
+// 		const finishTime = localStorage.getItem(KEY_TIME);
+// 	}catch (e) {
+// 		console.log("empty")
+// 	}
+// })();
 
