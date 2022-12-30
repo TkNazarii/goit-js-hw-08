678468e745350576bd30e417228b05e389038a24
TkNazarii <tknazkooo@gmail.com>
2022-12-30 15:55:25 +0200

    throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,17 +2,18 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const throttle = require('lodash.throttle');
 
 
 player.on('play', function() {
 	console.log('Пуск відео');
 });
 
-player.on('timeupdate', function(data) {
+player.on('timeupdate', throttle(function(data) {
 	console.log('Тривалість: '+data.seconds);
 	const asd = +data.seconds
 	localStorage.setItem("videoplayer-current-time", JSON.stringify(asd));
-});
+}, 1000));
 
 const savedSettings = localStorage.getItem("videoplayer-current-time");
 const parsedSettings = JSON.parse(savedSettings);
@@ -20,14 +21,4 @@ console.log(parsedSettings);
 
 player.setCurrentTime(parsedSettings);
 
-
-
-
-// (function(){
-// 	try{
-// 		const finishTime = localStorage.getItem(KEY_TIME);
-// 	}catch (e) {
-// 		console.log("empty")
-// 	}
-// })();
 
