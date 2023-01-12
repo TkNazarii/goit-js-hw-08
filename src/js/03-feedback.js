9d14936df9e335685ac722a667fcf504cec1f639
TkNazarii <tknazkooo@gmail.com>
2023-01-12 12:08:43 +0200

    finish8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,4 @@
+// обєкт ссилок
 const refs = {
 input: document.querySelector(".feedback-form input"),
 form: document.querySelector(".feedback-form"),
@@ -7,37 +8,38 @@ textarea: document.querySelector(".feedback-form textarea"),
 const STORAGE_KEY = "feedback-form-state";
 const throttle = require('lodash.throttle');
 
+// обєкт даних які заисуєм в локал
 let formData = {};
 
-
+// слухачі події
 refs.form.addEventListener("submit", onFormSubmit);
 refs.textarea.addEventListener("input", throttle(onInputValue, 500));
 refs.input.addEventListener("input", throttle(onInputValue, 500));
 refs.form.addEventListener("input", saveValue)
 populateTextarea();
 
-
-
-
+// дії при сабміті
 function onFormSubmit(event) {
 	event.preventDefault();
-	// console.log("Відправляю форму");
+	console.log("форма відправлення");
 	
 	event.currentTarget.reset();
 	localStorage.removeItem(STORAGE_KEY);
 	formData = {};
 };
 
+// записує в локал сторедж значення з інпута
 function onInputValue(event) {
 	const message = JSON.stringify(formData);
 
 	localStorage.setItem(STORAGE_KEY, message)
 };
 
+// повертає збережене значення з локал
 function populateTextarea() {
 	const saveMessage = localStorage.getItem(STORAGE_KEY);
 	const saveMessageParse = JSON.parse(saveMessage);
-	console.log(saveMessageParse); 
+	// console.log(saveMessageParse); 
 	
 	if (saveMessageParse) {
 		formData = saveMessageParse;
@@ -46,6 +48,7 @@ function populateTextarea() {
 	};
 };
 
+// у обєкті строрює назви ключів і значень
 function saveValue(event) {
 	// console.log(event.target.name);
 	// console.log(event.target.value);
